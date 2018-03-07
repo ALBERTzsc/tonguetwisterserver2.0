ed8f5118abe0457a19f611ae5bed6fd00116b31a
Albert <562494369@qq.com>
2018-03-07 22:32:05 +0800

    吧嗒绕口令服务端2.0初始化

---
+++ src/main/java/team/abc/tonguetwisterserver/TonguetwisterserverApplication.java
@@ -0,0 +1,12 @@
+package team.abc.tonguetwisterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonguetwisterserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonguetwisterserverApplication.class, args);
+	}
+}
